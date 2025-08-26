f24be8c29b2a104a1fd1b2330e9edeba7eba2d88
Sebastian Paral <sebastian.paral@knusperleicht.at>
2025-08-26 19:14:38 +0200

    Remove `phpunit-bootstrap.php` as it is no longer necessary.

--- phpunit-bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (file_exists('vendor/autoload.php'))
-    require_once 'vendor/autoload.php';
-else if (file_exists('../../autoload.php'))
-    require_once '../../autoload.php';
-else
-    echo "No autoload file found.";
